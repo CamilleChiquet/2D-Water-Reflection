9a4a42128f63ad7b242d5c6a9e26a20c85615fd1
CamilleCHIQUET <chiquetcamille@gmail.com>
2021-01-10 18:44:30 +0100

    Structure Updated

---
+++ Water Reflection/Editor/WaterReflectionEditor.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WaterReflection))]
+public class WaterReflectionEditor : Editor
+{
+    private WaterReflection waterReflection;
+    private SerializedProperty spriteRenderer;
+    private SerializedProperty camera;
+    private SerializedProperty waterShader;
+
+    private void OnEnable()
+    {
+        waterReflection = target as WaterReflection;
+
+        waterReflection.UpdateCamera();
+
+        spriteRenderer = serializedObject.FindProperty("spriteRenderer");
+        camera = serializedObject.FindProperty("camera");
+        waterShader = serializedObject.FindProperty("waterShader");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUI.BeginChangeCheck();
+
+        GUILayout.Space(10);
+        EditorGUILayout.PropertyField(camera, new GUIContent("Camera", "Camera used to capture the reflection scene."));
+        if (camera.objectReferenceValue == null)
+        {
+            EditorGUILayout.HelpBox("You must assign the camera.", MessageType.Error);
+        }
+        else
+        {
+            waterReflection.verticalCameraOffset = EditorGUILayout.FloatField(new GUIContent("Vertical Camera Offset", "By default, the camera used to capture the reflection's scene is placed just above the sprite renderer. You can adjust camera height by modifying this offset."), waterReflection.verticalCameraOffset);
+        }
+
+        GUILayout.Space(10);
+        EditorGUILayout.PropertyField(spriteRenderer, new GUIContent("Sprite Renderer", "Sprite that will receive the water shader."));
+        if (spriteRenderer.objectReferenceValue == null)
+        {
+            EditorGUILayout.HelpBox("You must assign the spriteRenderer.", MessageType.Error);
+        }
+        else
+        {
+            waterReflection.pixelsPerUnit = EditorGUILayout.IntField(new GUIContent("Pixels Per Unit", "Resolution of the reflection's texture."), waterReflection.pixelsPerUnit);
+            waterReflection.verticalSqueezeRatio = EditorGUILayout.FloatField(new GUIContent("Vertical Squeeze Ratio", "How much is the reflection squeezed vertically. 1 : no squeeze, > 1 : smaller reflection, < 1 : taller reflection."), waterReflection.verticalSqueezeRatio);
+        }
+
+        GUILayout.Space(10);
+        GUILayout.Label("Shader Parameters", EditorStyles.boldLabel);
+        EditorGUI.indentLevel++;
+        EditorGUILayout.PropertyField(waterShader, new GUIContent("Water Shader", "Shader used to simulate water."));
+        waterReflection.color = EditorGUILayout.ColorField(new GUIContent("Water Color", "Water's color."), waterReflection.color);
+        waterReflection.turbulencesStrength = EditorGUILayout.FloatField(new GUIContent("Turbulences Strength", "Strength of water's turbulences."), waterReflection.turbulencesStrength);
+        waterReflection.waterSpeed = EditorGUILayout.FloatField(new GUIContent("Water Speed", "Water's speed."), waterReflection.waterSpeed);
+        waterReflection.refraction = EditorGUILayout.FloatField(new GUIContent("Refraction/Reflection", "How much refraction (> 0) or Reflection(< 0) patterns are visible."), waterReflection.refraction);
+        waterReflection.noiseScale = EditorGUILayout.FloatField(new GUIContent("Noise Scale", "Scale of noise. Used to move and distord turbulences in a more realistic way."), waterReflection.noiseScale);
+        waterReflection.noisePower = EditorGUILayout.FloatField(new GUIContent("Noise Power", "Power given to noise. Used to move and distord turbulences in a more realistic way."), waterReflection.noisePower);
+        waterReflection.waveInversedScale = EditorGUILayout.Vector2Field(new GUIContent("Pattern Size Reduction", "Wave patterns inversed scale."), waterReflection.waveInversedScale);
+        EditorGUI.indentLevel--;
+
+        bool updateCamera = GUILayout.Button(new GUIContent("Force Visual Update", "If for some reason parameters are not applied, you can force them by clicking this button."));
+        if (updateCamera)
+        {
+            waterReflection.UpdateCamera();
+        }
+        if (EditorGUI.EndChangeCheck())
+        {
+            EditorUtility.SetDirty(waterReflection);
+            waterReflection.UpdateCamera();
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Water Reflection/Scripts/WaterReflection.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class WaterReflection : MonoBehaviour
+{
+    // Sprite that will receive the water shader.
+    public SpriteRenderer spriteRenderer;
+    // Camera used to capture the reflection scene.
+    public Camera camera;
+    // Resolution of the reflection's texture.
+    public int pixelsPerUnit = 32;
+    // Water's color.
+    public Color color;
+    // How much is the reflection squeezed vertically. 1 : no squeeze, > 1 : smaller reflection, < 1 : taller reflection.
+    public float verticalSqueezeRatio = 1;
+    // By default, the camera used to capture the reflection's scene is placed just above the sprite renderer. You can adjust camera height by modifying this offset.
+    public float verticalCameraOffset = 0;
+    // ======== Shader parameters ========
+    // Shader used to simulate water.
+    public Shader waterShader;
+    // Strength of water's turbulences.
+    public float turbulencesStrength = 0.4f;
+    // Water's speed.
+    public float waterSpeed = 0.01f;
+    // How much refraction (> 0) or Reflection(< 0) patterns are visible.
+    public float refraction = 0.5f;
+    // Scale of noise. Used to move and distord turbulences in a more realistic way.
+    public float noiseScale = 10;
+    // Power given to noise. Used to move and distord turbulences in a more realistic way.
+    public float noisePower = 0.03f;
+    // Wave patterns inversed scale.
+    public Vector2 waveInversedScale = Vector2.one;
+
+    private RenderTexture renderTexture;
+    private Material waterMaterial;
+
+    public void Awake()
+    {
+        // Unity modify camera's aspect when game starts (according to your screen dimensions). So we call UpdateCamera here to override it.
+        UpdateCamera();
+    }
+
+    public void UpdateCamera()
+    {
+        if (spriteRenderer != null && camera != null && waterShader != null)
+        {
+            renderTexture = new RenderTexture((int)spriteRenderer.bounds.size.x * pixelsPerUnit, (int)spriteRenderer.bounds.size.y * pixelsPerUnit, 1);
+            renderTexture.name = "Render Texture";
+
+            float cameraHeight = spriteRenderer.bounds.size.y * verticalSqueezeRatio;
+            camera.aspect = spriteRenderer.bounds.size.x / cameraHeight;
+            camera.orthographicSize = verticalSqueezeRatio * spriteRenderer.bounds.size.y / 2;
+            camera.targetTexture = renderTexture;
+            Vector3 cameraPosition = camera.transform.position;
+            cameraPosition.x = spriteRenderer.transform.position.x;
+            cameraPosition.y = verticalCameraOffset + spriteRenderer.transform.position.y + spriteRenderer.bounds.size.y / 2 + cameraHeight / 2;
+            camera.transform.position = cameraPosition;
+
+            waterMaterial = new Material(waterShader);
+            waterMaterial.SetTexture("_RenderTex", renderTexture);
+            waterMaterial.SetColor("_Color", color);
+            waterMaterial.SetFloat("_TurbulencesStrength", turbulencesStrength);
+            waterMaterial.SetFloat("_WaterSpeed", waterSpeed);
+            waterMaterial.SetFloat("_Refraction", refraction);
+            waterMaterial.SetFloat("_NoiseScale", noiseScale);
+            waterMaterial.SetFloat("_NoisePower", noisePower);
+            waterMaterial.SetVector("_PatternSizeReduction", waveInversedScale);
+
+            spriteRenderer.material = waterMaterial;
+        }
+    }
+}
